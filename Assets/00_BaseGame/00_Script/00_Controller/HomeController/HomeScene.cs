160ddfe8af62389e85f443f1279004cc21530f7c
Dung <vuquocdung204@gmail.com>
2025-09-10 22:10:36 +0700

    cp

--- Assets/00_BaseGame/00_Script/00_Controller/HomeController/HomeScene.cs
+++ Assets/00_BaseGame/00_Script/00_Controller/HomeController/HomeScene.cs
@@ -1,10 +1,20 @@
 
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HomeScene : MonoBehaviour
 {
+    public Button btnShop;
+    public Button btnRank;
+    public Button btnHome;
+    public Button btnTeam;
+    public Button btnCollection;
+    
     public void Init()
     {
         
     }
+
+    public Transform zoneSelectBird;
+    public Transform zoneClickPlay;
 }
