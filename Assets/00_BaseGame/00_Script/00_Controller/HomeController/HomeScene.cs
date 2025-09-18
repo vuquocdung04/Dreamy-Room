7c75bdb38f20e94b92e8f73704086e9a989a71a4
Dung <vuquocdung204@gmail.com>
2025-09-18 21:52:21 +0700

    cp

--- Assets/00_BaseGame/00_Script/00_Controller/HomeController/HomeScene.cs
+++ Assets/00_BaseGame/00_Script/00_Controller/HomeController/HomeScene.cs
@@ -4,11 +4,18 @@ using UnityEngine.UI;
 
 public class HomeScene : MonoBehaviour
 {
+    [Header("Navigation Bar")]
     public Button btnShop;
     public Button btnRank;
     public Button btnHome;
     public Button btnTeam;
     public Button btnCollection;
+    [Header("Other UI"), Space(5)]
+    public Button btnEditProfile;
+    public Button btnRemoveAds;
+    public Button btnSetting;
+    public Button btnDailyLogin;
+    public Button btnDailyReward;
     
     public void Init()
     {
